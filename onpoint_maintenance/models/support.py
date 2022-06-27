0e2de9fd9cf622cacea5198cac10e4b1aae83ff9
zunedisinambela <zunedisinambela@gmail.com>
2022-06-27 15:21:03 +0700

    State and StatusBar

--- onpoint_maintenance/models/support.py
+++ onpoint_maintenance/models/support.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import datetime
 
 
 class OnpointSupport(models.Model):
@@ -13,15 +14,33 @@ class OnpointSupport(models.Model):
     notes = fields.Text(required=True)
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('submitted', 'Submitted'),
-        ('approve', 'Approve'),
-        ('reject', 'Reject'),
+        ('submit', 'Submitted'),
+        ('approve', 'Approved'),
+        ('reject', 'Rejected'),
         ('in_process', 'In Process'),
-        ('complete', 'Complete')
-    ], default='draft',)
-    submit_date = fields.Date()
+        ('complete', 'Completed')
+    ], default='draft')
+    state_bug = fields.Selection(related='state')
+    state_request = fields.Selection(related='state')
+    state_approve = fields.Selection(related='state')
+    state_reject = fields.Selection(related='state')
+
+    submit_date = fields.Date(default=datetime.today())
     approve_reject_date = fields.Date()
     start_date = fields.Date()
     complete_date = fields.Date()
 
+    def action_submit(self):
+        self.state = 'submit'
 
+    def action_approve(self):
+        self.state = 'approve'
+
+    def action_reject(self):
+        self.state = 'reject'
+
+    def action_in_process(self):
+        self.state = 'in_process'
+
+    def action_complete(self):
+        self.state = 'complete'
