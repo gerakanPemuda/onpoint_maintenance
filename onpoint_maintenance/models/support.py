49e652fb4a7f89112ccbf0fa74cd9fa5deb8429c
zunedisinambela <zunedisinambela@gmail.com>
2022-06-28 13:49:42 +0700

    Fitur Button Date Today

--- onpoint_maintenance/models/support.py
+++ onpoint_maintenance/models/support.py
@@ -21,28 +21,32 @@ class OnpointSupport(models.Model):
         ('reject', 'Rejected'),
         ('in_process', 'In Process'),
         ('complete', 'Completed')
-    ], default='draft')
-    state_bug = fields.Selection(related='state')
-    state_request = fields.Selection(related='state')
-    state_approve = fields.Selection(related='state')
-    state_reject = fields.Selection(related='state')
+    ], tracking=True, default='draft', string='Status')
+    state_bug = fields.Selection(related='state', tracking=False)
+    state_request = fields.Selection(related='state', tracking=False)
+    state_approve = fields.Selection(related='state', tracking=False)
+    state_reject = fields.Selection(related='state', tracking=False)
 
     submit_date = fields.Date(default=datetime.now())
-    approve_reject_date = fields.Date(default=fields.Datetime.now, string='Approve/Reject Date')
-    start_date = fields.Date(default=fields.Datetime.now)
-    complete_date = fields.Date(default=fields.Datetime.now)
+    approve_reject_date = fields.Date()
+    start_date = fields.Date()
+    complete_date = fields.Date()
 
     def action_submit(self):
         self.state = 'submit'
 
     def action_approve(self):
         self.state = 'approve'
+        self.approve_reject_date = datetime.today()
 
     def action_reject(self):
         self.state = 'reject'
+        self.approve_reject_date = datetime.today()
 
     def action_in_process(self):
         self.state = 'in_process'
+        self.start_date = datetime.today()
 
     def action_complete(self):
         self.state = 'complete'
+        self.complete_date = datetime.today()
