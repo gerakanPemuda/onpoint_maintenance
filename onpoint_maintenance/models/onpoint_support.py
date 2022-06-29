4b72258ff53fd4e8698c7a8f948301d14bc8c762
zunedisinambela <zunedisinambela@gmail.com>
2022-06-29 15:04:55 +0700

    Fitur By

--- onpoint_maintenance/models/onpoint_support.py
+++ onpoint_maintenance/models/onpoint_support.py
@@ -7,6 +7,7 @@ class OnpointSupport(models.Model):
     _description = 'Onpoint Support'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'api.telegram.abstract']
 
+    name = fields.Char(string='Maintenance', required=True, copy=False, readonly=True, default=_('New'))
     support_type = fields.Selection([
         ('bug', 'Bug Fixing'),
         ('request', 'New Request'),
@@ -31,19 +32,28 @@ class OnpointSupport(models.Model):
     approve_reject_date = fields.Date()
     start_date = fields.Date()
     complete_date = fields.Date()
-    invoice = fields.Char(string='Maintenance', required=True, copy=False, readonly=True, default=_('New'))
 
     telegram_group_maintenance_ids = fields.Many2one('onpoint.telegram.group')
 
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('onpoint.support') or _('New')
+        res = super(OnpointSupport, self).create(vals)
+        return res
+
     def action_submit(self):
         self.state = 'submit'
-        message = '<b>' + self.support_type + '</b>' + "\n"
+        message = dict(self._fields['support_type'].selection).get(self.support_type) + "\n"
         message += (self.submit_date + timedelta(hours=7)).strftime('%d %B %y') + "\n"
         message += "\n"
         message += self.client_id.name + "\n"
         message += "\n"
         message += "Notes:" + "\n"
-        message += self.notes
+        message += self.notes + "\n\n"
+        message += "(by: " + self.env.user.name + ")" + "\n"
+        message += "\n"
+        message += "&#35;" + self.name
 
         params = {
             'chat_id': '413579342',
@@ -67,9 +77,4 @@ class OnpointSupport(models.Model):
         self.state = 'complete'
         self.complete_date = datetime.today()
 
-    @api.model
-    def create(self, vals):
-        if vals.get('invoice', _('New')) == _('New'):
-            vals['invoice'] = self.env['ir.sequence'].next_by_code('onpoint.support') or _('New')
-        res = super(OnpointSupport, self).create(vals)
-        return res
+
